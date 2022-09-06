597fa7ea9fe10e0069c75c7e92495d364ebe16d6
DavidLineman <dline215@gmail.com>
2022-09-06 15:11:24 -0400

    worked on edit and create forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Post was successfully created."
+      redirect_to dashboards_path
+    else
+      flash[:alert] = "Unable to save post."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @post = Post.find_by(params[:id])
   end
+
+  private 
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
   get 'dashboards/index', as: :dashboards
   resources :logins, only: [:new, :create]
   resources :registrations, only: [:new, :create]
+  resources :posts, only: [:new, :create, :edit]
 end
