26d42ae627cd4c66b0a44115d42c77925cd363cd
DavidLineman <dline215@gmail.com>
2022-09-08 10:09:10 -0400

    created show page and fixed routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboards/index', as: :dashboards
+  get 'posts/:id', to: 'posts#show'
   resources :logins, only: [:new, :create]
   resources :registrations, only: [:new, :create]
-  resources :posts, only: [:new, :create, :edit, :update]
+  resources :posts, only: [:new, :create, :update, :edit]
 end
